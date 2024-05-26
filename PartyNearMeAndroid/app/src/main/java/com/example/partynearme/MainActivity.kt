46e5b050a0428adf903fa474cd28c076ecaed72b
Mateo Rodriguez <pbes159@gmail.com>
2024-05-26 18:34:43 +0100

    android app in progress

---
+++ PartyNearMeAndroid/app/src/main/java/com/example/partynearme/MainActivity.kt
@@ -0,0 +1,58 @@
+package com.example.partynearme
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Button
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.rememberNavController
+import com.example.partynearme.ui.theme.PartyNearMeTheme
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            PartyNearMeTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background) {
+                    appNavigator()
+                }
+            }
+        }
+    }
+}
+@Composable
+fun appNavigator(){
+    val navController = rememberNavController()
+    NavHost(navController = navController, startDestination = "main"){
+        composable("main"){
+            greeting("Android", navController, Modifier)
+        }
+        composable("forYou"){
+            forYouScreen()
+        }
+    }
+}
+@Composable
+fun greeting(name: String, navController: NavHostController, modifier: Modifier) {
+Button(onClick = { navController.navigate("forYou") }){
+    Text(text = "Go to For You Screen")
+}
+}
+
+@Preview(showBackground = true)
+@Composable
+fun greetingPreview() {
+    PartyNearMeTheme {
+        greeting("Android", rememberNavController(), Modifier)
+    }
+}
---
+++ PartyNearMeAndroid/app/src/main/java/com/example/partynearme/forYouScreen.kt
@@ -0,0 +1,9 @@
+package com.example.partynearme
+
+import androidx.compose.runtime.Composable
+import androidx.compose.material3.Text
+
+@Composable
+fun forYouScreen() {
+    Text(text = "This is the For You Screen!")
+}
