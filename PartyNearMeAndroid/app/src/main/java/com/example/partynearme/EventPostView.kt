149ce1e2e4491878341e1e3ac7cd08e6136ded4b
Mateo Rodriguez <pbes159@gmail.com>
2024-05-29 22:47:15 +0100

    ForYouPage centered

--- PartyNearMeAndroid/app/src/main/java/com/example/partynearme/EventPostView.kt
+++ PartyNearMeAndroid/app/src/main/java/com/example/partynearme/EventPostView.kt
@@ -1,6 +1,7 @@
 package com.example.partynearme
 
 import androidx.compose.foundation.layout.Column
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 
 @Composable
--- PartyNearMeAndroid/app/src/main/java/com/example/partynearme/forYouScreen.kt
+++ PartyNearMeAndroid/app/src/main/java/com/example/partynearme/forYouScreen.kt
@@ -1,8 +1,16 @@
 package com.example.partynearme
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.Modifier
 import androidx.compose.material3.Text
+import androidx.compose.ui.Alignment
+import androidx.compose.foundation.layout.Arrangement
 
 @Composable
 fun forYouScreen() {
@@ -11,9 +19,24 @@ fun forYouScreen() {
         EventPost("2", "Event 2", null, null, null, "Location 2"),
         // More posts to be added here
     )
-    LazyColumn {
-        items(dummyPosts) { post ->
-            EventPostView(post)
+    Column(
+        modifier = Modifier.fillMaxWidth(),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Top
+    ){
+        Text(
+            text = "For You",
+            textAlign = TextAlign.Center
+        )
+        LazyColumn {
+            items(dummyPosts) { post ->
+                EventPostView(post)
+            }
         }
     }
+}
+@Preview(showBackground = true)
+@Composable
+fun PreviewForYouScreen() {
+    forYouScreen()
 }
