6c765eac14a2f89467ef5dd85c484294462c6c41
Mateo Rodriguez (TMETC Plc) <mateo.rodriguez@tatamotors.com>
2024-05-30 16:00:32 +0100

    Remote pc work

--- PartyNearMeAndroid/app/src/main/java/com/example/partynearme/EventPostView.kt
+++ PartyNearMeAndroid/app/src/main/java/com/example/partynearme/EventPostView.kt
@@ -1,12 +1,19 @@
 package com.example.partynearme
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Column
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.res.painterResource
 
 @Composable
 fun EventPostView(eventPost: EventPost) {
     Column {
+        Row {
+            Image(painterResource(id = R.drawable.profile_pic), contentDescription = null)
+            Text("Username")
+        }
         Text(text = eventPost.text)
         eventPost.music?.let {
             //music will be displayed here
